9031d001e28a4aea4de33834f964a8b9f34c77a5
Ivan Sheremeta <sheremetavanya2004@gmail.com>
2024-08-19 11:02:09 +0300

    add: finished log-in page

---
+++ client/src/components/Button.jsx
@@ -0,0 +1,25 @@
+import clsx from "clsx";
+import PropTypes from "prop-types";
+
+const Button = ({ icon, className, label, type, onClick = () => {} }) => {
+  return (
+    <button
+      type={type || "button"}
+      className={clsx("bx-3 py-2 outline-none", className)}
+      onClick={onClick}
+    >
+      <span>{label}</span>
+      {icon && icon}
+    </button>
+  );
+};
+
+Button.propTypes = {
+  icon: PropTypes.element,
+  className: PropTypes.string,
+  label: PropTypes.string.isRequired,
+  type: PropTypes.oneOf(["button", "submit", "reset"]),
+  onClick: PropTypes.func,
+};
+
+export default Button;
